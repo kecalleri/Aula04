ddd16284c4eedbd86b664fa5632446a95fa84f5a
Karen Calleri <ke.calleri@gmail.com>
2021-06-12 15:23:55 -0300

    Implementado o loop while

---
+++ src/EntendendoWhile.java
@@ -0,0 +1,14 @@
+
+public class EntendendoWhile {
+
+	public static void main(String[] args) {
+		int contador=0;
+		
+		while(contador<10) {
+			System.out.println("Mensagem");
+			contador = contador + 1;
+		}
+
+	}
+
+}
